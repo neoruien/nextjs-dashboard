d296eddc19cc548a6dec7628ce088d185b2001f7
neoruien <neoruien123@gmail.com>
2024-12-29 21:56:55 +0800

    feat: implement form validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,9 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { State, updateInvoice } from '@/app/lib/actions';
+import { useActionState } from 'react';
+import ErrorMessages from './errors';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,11 +20,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
   // Have to do this, because we cannot pass id as an argument in form action (i.e. action={updateInvoice(id)})
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -64,9 +68,11 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <ErrorMessages errors={state.errors} id='amount' />
           </div>
         </div>
 
---
+++ app/ui/invoices/errors.tsx
@@ -0,0 +1,17 @@
+import { Errors } from "@/app/lib/actions";
+
+export default function ErrorMessages({ errors, id }: {
+    errors: Errors | undefined,
+    id: keyof Errors,
+}) {
+    const errMsg: string[] = !errors ? [] : errors[id] ?? [];
+    return (
+        <div id={`${id}-error`} aria-live="polite" aria-atomic="true">
+            {errMsg.map((error) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                </p>
+            ))}
+        </div>
+    );
+};
