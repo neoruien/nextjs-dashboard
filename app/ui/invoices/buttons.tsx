7aa23618be080cd6694b250c32d4a83bb3fa065a
neoruien <neoruien123@gmail.com>
2025-01-06 18:33:25 +0800

    fix: ignore ts error

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -29,6 +29,7 @@ export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
   return (
+    // @ts-expect-error typing issue
     <form action={deleteInvoiceWithId}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
