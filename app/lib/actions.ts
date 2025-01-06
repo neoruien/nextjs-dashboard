60222729528916c614dd78b6a5c6103e279b9c28
neoruien <neoruien123@gmail.com>
2025-01-06 12:31:40 +0800

    fix: eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,6 +60,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
     } catch (error) {
         // If a database error occurs, return a more specific error.
+        console.error(error);
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
@@ -100,7 +101,8 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
     } catch (error) {
-        return { message: 'Database Error: Failed to Update Invoice.' };
+        console.error(error);
+        return { message: "Database Error: Failed to Update Invoice." };
     }
 
     revalidatePath('/dashboard/invoices');
